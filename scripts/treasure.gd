cfbb455bc31fc77a81580c66864f7a385d84d690
Dany <danielgeorgewadea@gmail.com>
2025-11-24 06:29:40 +0200

    Add Framework parts to treasures and update levels
    Added new asset images for CPU, GPU, RAM, SSD, HDD, and Framework, and updated all level scenes to display the corresponding treasure item image and name. Also adjusted level 6 to use 'Framework' as the treasure and updated the levels menu with a new main label and font. Added few enhancements and QOL updates.

--- scripts/treasure.gd
+++ scripts/treasure.gd
@@ -1,13 +1,24 @@
 extends Node2D
 
+@export var item: String
+@export var next_scene:String
+
 @onready var game_manager: Node2D = %GameManager
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 @onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var win_label: Label = $WinLabel
 @onready var lose_label: Label = $LoseLabel
 @onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
-@export var next_scene:String
+func _ready() -> void:
+	lose_label.hide()
+	win_label.hide()
+	sprite_2d.hide()
+	if get_tree().current_scene.name == "Level6":
+		win_label.text = "You win\nYou assembled " + item
+	else:
+		win_label.text += " " + item
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
@@ -20,6 +31,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 			collision_shape_2d.queue_free()
 			lose_label.hide()
 			win_label.show()
+			sprite_2d.show()
 			animation.play("opened")
 			audio_player.play()
 			Engine.time_scale = 0.5
